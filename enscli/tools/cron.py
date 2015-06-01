beea40c9639d346ee8bb2b3e81b008e342e06b2b
Dominick Rivard <drivard@auto123.com>
2015-06-01 15:18:54 -0400

    CLI --> trying to save cron

--- enscli/tools/cron.py
+++ enscli/tools/cron.py
@@ -28,8 +28,9 @@ def create_a_cron(ttl, action, comment):
     custom_cmd = executable + ' ' + action
 
     job = cron.new(command=custom_cmd, comment=comment)
-    job.minute.every(ttl / 60)
+    job.minute().every(ttl / 60)
     job.enable()
+    job.cron.write()
 
     return job.is_valid()
 
